00513353dfce965d83056f8546e4cff71eecc968
Hugo Moran <moranh56@gmail.com>
2022-03-31 20:50:27 -0600

    Add display buildings in 3D

--- src/components/MapboxMap.ts
+++ src/components/MapboxMap.ts
@@ -7,8 +7,11 @@ class MapboxMap extends HTMLElement {
         const map = new mapboxgl.Map({
             container: this,
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-90.513604, 14.641601], // [lon, lat]
-            zoom: 9
+            center: [-74.0066, 40.7135],
+            zoom: 17,
+            pitch: 0,
+            bearing: -17.6,
+            antialias: true
         });
 
         const nav = new mapboxgl.NavigationControl();
@@ -23,6 +26,59 @@ class MapboxMap extends HTMLElement {
         scale.setUnit('metric');
 
         map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body') }));
+
+        map.on('load', () => {
+            // Insert the layer beneath any symbol layer.
+            const layers = map.getStyle().layers;
+            const labelLayerId = layers?.find(
+                (layer) => layer.type === 'symbol' && layer?.layout && layer?.layout['text-field']
+            )?.id;
+
+            map.addLayer(
+                {
+                    id: 'add-3d-buildings',
+                    source: 'composite',
+                    'source-layer': 'building',
+                    filter: ['==', 'extrude', 'true'],
+                    type: 'fill-extrusion',
+                    minzoom: 15,
+                    paint: {
+                        'fill-extrusion-color': '#aaa',
+                        'fill-extrusion-height': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            15,
+                            0,
+                            15.05,
+                            ['get', 'height']
+                        ],
+                        'fill-extrusion-base': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            15,
+                            0,
+                            15.05,
+                            ['get', 'min_height']
+                        ],
+                        'fill-extrusion-opacity': 1
+                    }
+                },
+                labelLayerId
+            );
+
+            map.addLayer({
+                id: 'sky',
+                type: 'sky',
+                paint: {
+                    'sky-type': 'atmosphere',
+                    'sky-atmosphere-sun': [0.0, 0.0],
+                    'sky-atmosphere-sun-intensity': 15
+                }
+            });
+
+        });
 
     }
 };
